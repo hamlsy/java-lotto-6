6513e61974941d5ff940b965863640fd3b89f2c0
hamlsy <dltmddud1122@naver.com>
2024-09-18 23:55:54 +0900

    feat: 프로젝트 구조, 클래스 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,24 @@
+package lotto.controller;
+
+public class LottoController {
+
+    //todo create start method
+    // before lotto, start lotto, result lotto impl
+
+    public void start(){
+        // input lotto
+        // start lotto
+        // result lotto
+    }
+
+
+    //todo create input lotto(before lotto) method
+
+
+    //todo create start lotto method
+
+
+    //todo create result lotto method
+
+
+}
